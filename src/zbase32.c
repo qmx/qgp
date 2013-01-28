24c052aea68883f84f4b2ea6a3d66ee717714ee7
Douglas Campos <qmx@qmx.me>
2013-01-28 01:38:44 -0200

    put zbase32 into a library

---
+++ src/zbase32.c
@@ -0,0 +1,8 @@
+#include "zbase32.h"
+
+int zbase32_dec(const uint8_t *input, uint8_t *output, int outputLen) {
+    return -1;
+}
+int zbase32_enc(const uint8_t *input, int len, uint8_t *output, int outputLen) {
+    return -1;
+}
